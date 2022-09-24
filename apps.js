e551a30dc98053b48c2fd64d688e0cb0a3344df8
kelvinirungu <kelvinn.irungu69@gmail.com>
2022-09-24 11:12:31 +0300

    app.js update

--- apps.js
+++ apps.js
@@ -1,1 +1,34 @@
 const form = document.querySelector('form');
+const text =document.getElementById('text');
+const todoCon = document.querySelector('.todo-con');
+
+
+form.addEventListener('submit', (e)=>
+{
+e.preventDefault();
+function addtodo() {
+  let todoDowm = document.createElement('div');
+  let todotext = text.value;
+  todoDowm.innerHTML = `
+  <div class="todo-li">
+  <div class="check tick-active">
+    <img src="./images/icon-check.svg" alt="" />
+  </div>
+  <p>${todotext}</p> 
+  <div class="close-edit">
+    <p class="edit-btn">Edit</p>
+    <button class="close">
+      <img src="./images/icon-cross.svg" alt="" />
+    </button>
+  </div>
+</div>
+<div class="break"></div>
+  `
+  todoCon.appendChild(todoDowm);
+  
+}
+
+addtodo();
+
+});
+

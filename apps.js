3a9643545583d7eb7402767cc2c32d30ea1a417c
kelvinirungu <kelvinn.irungu69@gmail.com>
2022-09-26 16:40:30 +0300

    local storage js

--- apps.js
+++ apps.js
@@ -6,15 +6,35 @@ const todoCon = document.querySelector('.todo-con');
 form.addEventListener('submit', (e)=>
 {
 e.preventDefault();
-function addtodo() {
+
+ addtodo();
+
+});
+
+
+let storagetext =JSON.parse(localStorage.getItem("storagetext"));
+
+if(storagetext) {
+//  storagetext=JSON.pars   e(storagetext);
+ storagetext.forEach(item => {
+   addtodo(item)
+
+ });
+}
+
+function addtodo(textSt) {
   let todoDowm = document.createElement('div');
   let todotext = text.value;
+  if (textSt){
+    todotext=textSt.text;
+  }
+if(todotext){
   todoDowm.innerHTML = `
   <div class="todo-li">
-  <div class="check ">
+  <div class="check " >
     <img src="./images/icon-check.svg" alt="" />
   </div>
-  <p>${todotext}</p> 
+  <p class = "store" >${todotext}</p> 
   <div class ="edit-btn"> Edit</div>
   <div class="close-edit">
     <button class="close">
@@ -23,11 +43,15 @@ function addtodo() {
   </div>
 </div>
 <div class="break"></div>
-  `
+  `;
   todoCon.appendChild(todoDowm);
+
+  update ();
+}
   const close = todoDowm.querySelector('.close');
   close.addEventListener('click', () => {
     todoDowm.remove();
+    
   });
 
   const check = todoDowm.querySelector('.check');
@@ -35,11 +59,20 @@ function addtodo() {
   check.addEventListener('click', ()=> {
     check.classList.toggle('tick-active')
     todoDowm.children[0].children[1].classList.toggle('completed')
+    update ();
   });
   
 }
 
-addtodo();
 
-});
-
+function update () {
+  const store =document.querySelectorAll('.store');
+  let arr = [];
+  store.forEach(item=>{
+arr.push({
+  text:item.innerText,
+  complete:item.classList.contains("completed")
+})
+  });
+  localStorage.setItem("storagetext", JSON.stringify(arr));
+}
